e5880b1ca72ada22526b0c57d2d1151fee2f8645
samrah10 <152185620+samrah10@users.noreply.github.com>
2026-04-18 17:43:16 -0400

    Reduce scan interval from 10000ms to 7000ms

--- public/app.js
+++ public/app.js
@@ -20,7 +20,7 @@ let goalConfirmedFor = "";
 let lastSpokenMessage = "";
 let lastSpokenTime = 0;
 
-const SCAN_INTERVAL_MS = 10000;
+const SCAN_INTERVAL_MS = 7000;
 const DUPLICATE_SPEECH_WINDOW_MS = 8000;
 
 function setStatus(message, kind = "warn") {
